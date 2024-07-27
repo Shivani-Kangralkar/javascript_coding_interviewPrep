b5d861cdcb2bc1ec54279447afa51f61a6730d6f
Shivani Kangralkar <shivanikangralkar7@gmail.com>
2024-07-27 13:51:49 +0530

    Find code on memoization , where same values are passed data is fetched from cache, if different value are passed than sum is being calculated.

---
+++ memoization.js
@@ -0,0 +1,29 @@
+
+
+
+const sum = (a,b)=>{
+    return a + b
+}
+
+function memo(fn){
+
+    const cache = {};
+    return function(...args){
+
+        const key = args.join(",")
+        if(cache[key]){
+            console.log("feteched value from cache", key)
+            return cache[key]
+        }else{
+            const result = fn.apply(this,args)
+            cache[key] = result
+            return result
+        }
+
+    }
+
+}
+
+const result = memo(sum);
+console.log(result(2,2))
+console.log(result(2,2))
